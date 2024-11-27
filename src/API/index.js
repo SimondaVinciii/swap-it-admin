1e02a77ecb9e4e5f88210a9e2b3deabe743e8eb1
nhanralph <dnhnhan2003@gmail.com>
2024-11-27 16:29:39 +0700

    fix

--- src/API/index.js
+++ src/API/index.js
@@ -17,14 +17,14 @@ export const getComments = () => {
   return fetch("https://dummyjson.com/comments").then((res) => res.json());
 };
 export const getUsers = () => {
-  // return fetch("https://swap-it-api.onrender.com/api/v1/users").then((res) => res.json());
-  return fetch("http://localhost:3002/api/v1/users").then((res) => res.json());
+  return fetch("https://swap-it-api.onrender.com/api/v1/users").then((res) => res.json());
+  // return fetch("http://localhost:3002/api/v1/users").then((res) => res.json());
 }
 export const getItems = () => {
-  // return fetch("https://swap-it-api.onrender.com/api/v1/items").then((res) => res.json());
-  return fetch("http://localhost:3002/api/v1/items").then((res) => res.json());
+  return fetch("https://swap-it-api.onrender.com/api/v1/items").then((res) => res.json());
+  // return fetch("http://localhost:3002/api/v1/items").then((res) => res.json());
 }
 export const getTransactions = () => {
-  // return fetch("https://swap-it-api.onrender.com/api/v1/transactions").then((res) => res.json());
-  return fetch("http://localhost:3002/api/v1/transactions").then((res) => res.json());
+  return fetch("https://swap-it-api.onrender.com/api/v1/transactions").then((res) => res.json());
+  // return fetch("http://localhost:3002/api/v1/transactions").then((res) => res.json());
 }
